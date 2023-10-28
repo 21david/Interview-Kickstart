fc873536d8e39836681ca61ca2caea2d4ec9b992
David <david.espinosa.21@gmail.com>
2023-10-27 21:20:26 -0500

    Update Build a Balanced BST From a Sorted Array.py
    Added an optimized approach.

--- Week 3 - Trees/Build a Balanced BST From a Sorted Array.py
+++ Week 3 - Trees/Build a Balanced BST From a Sorted Array.py
@@ -12,6 +12,15 @@ class BinaryTreeNode:
 The list will always have at least one node.
 '''
 
+'''
+To build a balanced search tree using a sorted
+array, then we can take the middle element, make
+that one the root, and then call a helper function
+(the current function in a recursive way)
+to build BSTs from all the elements to the left 
+and to the right, which will be the left and right
+children.
+'''
 def build_balanced_bst(a):
     """
     Args:
@@ -19,15 +28,6 @@ def build_balanced_bst(a):
     Returns:
      BinaryTreeNode_int32
     """
-    '''
-    To build a balanced search tree using a sorted
-    array, then we can take the middle element, make
-    that one the root, and then call a helper function
-    (the current function in a recursive way)
-    to build BSTs from all the elements to the left 
-    and to the right, which will be the left and right
-    children.
-    '''
     length = len(a)
     
     if length == 0:
@@ -41,3 +41,38 @@ def build_balanced_bst(a):
     root.right = build_balanced_bst(a[middle_index+1:])
     
     return root
+
+
+
+'''
+An optimization to the previous solution is to 
+use the original array for each recursive call 
+along with start and end pointers that indicate 
+the subset of the array to build a BST from. 
+This reduces the auxilliary space used by about
+half.
+'''
+def build_balanced_bst(a):
+    """
+    Args:
+     a(list_int32)
+    Returns:
+     BinaryTreeNode_int32
+    """
+    
+    def helper(arr, start, end):
+        length = end - start + 1
+        
+        if length == 0:
+            return None
+        
+        middle_index = start + (length) // 2
+        root_element = arr[middle_index]
+        root = BinaryTreeNode(root_element)
+        
+        root.left = helper(arr, start, middle_index-1)
+        root.right = helper(arr, middle_index+1, end)
+        
+        return root
+    
+    return helper(a, 0, len(a)-1)
