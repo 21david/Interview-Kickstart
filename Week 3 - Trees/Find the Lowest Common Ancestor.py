97e32ef4258d57a593df374df8e9fdf494c0225e
David <david.espinosa.21@gmail.com>
2023-11-08 18:18:02 -0500

    Update Find the Lowest Common Ancestor.py
    Added two more solutions I wrote.

--- Week 3 - Trees/Find the Lowest Common Ancestor.py
+++ Week 3 - Trees/Find the Lowest Common Ancestor.py
@@ -45,6 +45,7 @@ def helper(root, p, q):
     if not root:
         return None
 
+    # This becomes true if the current node is one of the two input nodes
     found_node = root.value == p.value or root.value == q.value
 
     if found_node:
@@ -63,3 +64,165 @@ def helper(root, p, q):
         return root
     elif left or right:
         return left or right
+
+
+
+'''
+This approach uses a nested DFS function and a global variable that
+holds the LCA node when it is found. When it is found, it stops
+serching and returns the answer.
+
+We can do one pass through the whole tree, keeping track of 3 things:
+1. Is the current node either p or q
+2. How many nodes (p and/or q) we found on the left subtree
+3. How many nodes we found on the right subtree
+
+If the current node is either p or q, then there are 2 possible cases:
+1. If the other node is somewhere on the left or right subtrees, then
+   the current node is the LCA
+2. If the other node isn't somwhere on the left or right subtree, then
+   the current node is not the LCA
+
+If the current node is not either p or q, then there are 2 cases:
+1. One node is on the left subtree and the other one is in the right subtree.
+   If this is the case, then the current node is the LCA.
+2. Both nodes are either on the left subtree or on the right subtree. If so,
+   we continue the traversal on both sides. 
+
+After doing one pass, we should have found the answer. An optimization is,
+as soon as we find the LCA, we can stop the traversal from continuing.
+
+Also, if a and b are the same, then they are their own LCA, so we can return 
+without doing any searching.
+
+Time time complexity of this is O(N) because we visit each node once.
+The space complexity is O(N) because if the tree is a linear chain of nodes,
+the recursion stack call may reach a height of N.
+'''
+"""
+For your reference:
+class BinaryTreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+"""
+def lca(root, a, b):
+    """
+    Args:
+     root(BinaryTreeNode_int32)
+     a(BinaryTreeNode_int32)
+     b(BinaryTreeNode_int32)
+    Returns:
+     int32
+    """
+    
+    if a.value == b.value:
+        return a.value
+    
+    lca = [None]
+    
+    def dfs(node):
+        if lca[0] or not node:
+            return 0
+    
+        left = dfs(node.left)
+        right = dfs(node.right)
+    
+        found = node.value == a.value or node.value == b.value
+        if found:
+            if left == 1 or right == 1:
+                # current node is LCA
+                lca[0] = node
+                return 0
+            else:
+                return left | right | found
+                
+        else:
+            if left == 1 and right == 1:
+                lca[0] = node
+                return 0
+            else:  
+                return left | right
+    
+    dfs(root)
+    return lca[0].value
+
+
+
+'''
+This approach is correct, but has a much worse time complexity
+of O(N^2), because in the worst case, it starts a DFS at almost every 
+node in the tree.
+
+If we start from the root and search the left subtree, then 3 things
+can happen:
+1. we find only one node (p or q)
+2. we find both nodes
+3. we find neither node
+
+In case 1, then the root node is the LCA, because the other node
+is in the right subtree.
+In case 2, there is a lower LCA, so we should start searching
+again from the left child.
+Case 3 is basically the mirrored version of case 2. There is a lower LCA
+somewhere on the right subtree, so we can start searching from the
+right child.
+
+The search will check the current node itself, since nodes can be 
+descendants/ancestors of themselves.
+
+Our algorithm could start a DFS at the root node, which will be a function 
+that searches for both nodes, and returns how many it found.
+Based off of that, it will return or repeat the search on the left
+or right child until it finds it.
+'''
+"""
+For your reference:
+class BinaryTreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+"""
+def lca(root, a, b):
+    """
+    Args:
+     root(BinaryTreeNode_int32)
+     a(BinaryTreeNode_int32)
+     b(BinaryTreeNode_int32)
+    Returns:
+     int32
+    """
+    if a.value == b.value:
+        return a.value
+
+    # Count how many input nodes were found in a DFS. Either 0, 1, or 2.
+    count = [0]
+
+    def dfs(node):
+        if not node or count[0] == 2:
+            return
+        
+        if node.value == a.value or node.value == b.value:
+            count[0] += 1
+            
+        dfs(node.left)
+        dfs(node.right)
+    
+
+    while root:
+        if root.value == a.value or root.value == b.value:
+            return root.value
+
+        count[0] = 0
+        
+        dfs(root.left)
+
+        if count[0] == 1:
+            return root.value
+        elif count[0] == 0:
+            root = root.right
+        else:  # result == 2
+            root = root.left
+            
