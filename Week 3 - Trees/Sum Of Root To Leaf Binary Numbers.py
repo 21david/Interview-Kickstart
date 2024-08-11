670d98dbf2ca02027033bf0c89c32c363158dff0
David <david.espinosa.21@gmail.com>
2024-08-11 10:58:24 -0500

    Update Sum Of Root To Leaf Binary Numbers.py

--- Week 3 - Trees/Sum Of Root To Leaf Binary Numbers.py
+++ Week 3 - Trees/Sum Of Root To Leaf Binary Numbers.py
@@ -38,3 +38,38 @@ def sum_root_to_leaf_numbers(root):
         
     dfs(root, [])
     return total
+
+
+'''
+More space efficient solution
+using bitwise operators and math.
+Same approach as above.
+'''
+def sum_root_to_leaf_numbers(root):
+    """
+    Args:
+     root(BinaryTreeNode_int32)
+    Returns:
+     int32
+    """
+    total = 0
+    
+    def dfs(node, num):
+        nonlocal total
+        if not (node.left or node.right):
+            num <<= 1
+            num += node.value
+            total += num
+            num >>= 1
+            return
+            
+        num <<= 1
+        num += node.value
+        if node.left:
+            dfs(node.left, num)
+        if node.right:
+            dfs(node.right, num)
+        num >>= 1
+        
+    dfs(root, 0)
+    return total
